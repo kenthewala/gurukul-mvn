fb6d86270713fdfa89438c031a4eddb4c13c6a11
kenthewala <anoopsinghkhimani@gmail.com>
2014-12-09 21:31:49 +0530

    Address Embeddable

---
+++ src/main/java/com/anoop/gurukul/util/Address.java
@@ -0,0 +1,103 @@
+package com.anoop.gurukul.util;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+
+@Embeddable @Access(AccessType.FIELD)
+public class Address {
+	private int doorNumber;
+	private String streetName;
+	private String locality;
+	private String area;
+	private String landmark;
+	private String city;
+	private String state;
+	/**
+	 * @return the doorNumber
+	 */
+	public int getDoorNumber() {
+		return doorNumber;
+	}
+	/**
+	 * @param doorNumber the doorNumber to set
+	 */
+	public void setDoorNumber(int doorNumber) {
+		this.doorNumber = doorNumber;
+	}
+	/**
+	 * @return the streetName
+	 */
+	public String getStreetName() {
+		return streetName;
+	}
+	/**
+	 * @param streetName the streetName to set
+	 */
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	/**
+	 * @return the locality
+	 */
+	public String getLocality() {
+		return locality;
+	}
+	/**
+	 * @param locality the locality to set
+	 */
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+	/**
+	 * @return the area
+	 */
+	public String getArea() {
+		return area;
+	}
+	/**
+	 * @param area the area to set
+	 */
+	public void setArea(String area) {
+		this.area = area;
+	}
+	/**
+	 * @return the landmark
+	 */
+	public String getLandmark() {
+		return landmark;
+	}
+	/**
+	 * @param landmark the landmark to set
+	 */
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+
+}
